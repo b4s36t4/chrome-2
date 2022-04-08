daeba28bf24f204d4425ba48f46de7e7d26d6d8b
Mahesh Vagicherla <maheshvagicherla99438@gmail.com>
2022-04-08 14:52:24 +0530

    added accounts & meet link

--- src/pages/Background/index.js
+++ src/pages/Background/index.js
@@ -1,2 +1,56 @@
-console.log('This is the background page.');
-console.log('Put the background scripts here.');
+const getEmails = () => {
+    const mailNodes = document.querySelectorAll(".wLBAL")
+    const mails = []
+    mailNodes.forEach((node) => mails.push(node.textContent))
+    console.log(mails)
+    return { "mails": mails }
+}
+
+
+const choose_accounts = (callBack) => {
+    chrome.tabs.create({ active: false, url: "https://accounts.google.com/accountchooser" }, (tab) => {
+        if (tab) {
+            chrome.scripting.executeScript({ target: { tabId: tab?.id }, func: getEmails }, async (res) => {
+                if (typeof res === "object") {
+                    const mailResult = res[0]
+                    if (mailResult) {
+                        const mails = mailResult?.result["mails"]
+                        await chrome.storage.local.set({ "accounts": JSON.stringify(mails) })
+                        chrome.tabs.remove(tab?.id)
+                    }
+                }
+                callBack(true)
+            })
+        }
+        callBack(true)
+    })
+}
+
+// chrome.webNavigation.onCompleted.addListener((details) => {
+//     chrome.runtime.sendMessage({ type: "meet_url", "url": details.url })
+//     return true;
+// }, { url: [{ urlContains: "meet" }] })
+
+chrome.runtime.onMessage.addListener((msg, _, callBack) => {
+    console.log("Got a message")
+    if (typeof msg === "object") {
+        const { type } = msg
+        if (type === "choose_accounts") {
+            choose_accounts(callBack)
+        }
+        if (type === "create_meet") {
+            const { link } = msg
+            chrome.tabs.create({ url: link, active: false })
+                .then((tab) => {
+                    if (tab) {
+                        // callBack(true)
+                        chrome.webNavigation.onCompleted.addListener((details) => {
+                            if (details && details.tabId) {
+                                callBack(details.url)
+                            }
+                        }, { url: [{ urlContains: "meet.google.com" }] })
+                    }
+                })
+        }
+    }
+})
--- src/pages/Popup/Popup.jsx
+++ src/pages/Popup/Popup.jsx
@@ -1,25 +1,115 @@
-import React from 'react';
-import logo from '../../assets/img/logo.svg';
-import Greetings from '../../containers/Greetings/Greetings';
+import React, { useEffect, useState } from 'react';
 import './Popup.css';
 
 const Popup = () => {
+  const [selectedAccount, setSelectAccount] = useState();
+  const [availableAccounts, setAvailableAccounts] = useState();
+  const [accountIndex, setAccountIndex] = useState(0);
+  useEffect(() => {
+    const call = async () => {
+      const accounts = await chrome.storage.local.get("accounts")
+      if (!accounts || Object.entries(accounts).length < 1) {
+        console.log("No Saved Accounts")
+      } else {
+        setAvailableAccounts(JSON.parse(accounts['accounts']))
+      }
+    }
+    call()
+  }, [])
+  useEffect(() => {
+    const call = async () => {
+      const selectedOne = await chrome.storage.local.get("selected_account")
+
+      if (!selectedOne || Object.entries(selectedOne).length < 1) {
+        setSelectAccount("")
+      } else {
+        setSelectAccount(selectedOne['selected_account'])
+      }
+    }
+    call()
+  }, [])
+
+  useEffect(() => {
+    chrome.storage.onChanged.addListener((changes, namespace) => {
+      if (namespace === "local") {
+        const keys = Object.keys(changes)
+        const isRelaodable = keys.includes("accounts") || keys.includes("selected_account") || keys.includes("account_index")
+        if (isRelaodable) {
+          window.location.reload()
+        }
+      }
+    })
+
+    chrome.runtime.onMessage.addListener((msg, sender) => {
+      console.log(msg, "asdas")
+      return true;
+    })
+  }, [])
+
+  const chooseAccount = async () => {
+    console.log(accountIndex)
+    if (accountIndex !== undefined) {
+      const account = availableAccounts[accountIndex];
+      console.log(account)
+      await chrome.storage.local.set({ "selected_account": account })
+      await chrome.storage.local.set({ "account_index": accountIndex })
+      // window.location.reload()
+    } else return
+  }
+
+  const deleteAccount = async () => {
+    await chrome.storage.local.clear()
+    // window.location.reload();
+  }
+  const doLogin = async () => {
+    chrome.runtime.sendMessage({ type: "choose_accounts" }, (res) => {
+      console.log(res)
+    })
+  }
+
+  const createMeet = async () => {
+    if (selectedAccount) {
+      const accountIndex = await chrome.storage.local.get(["account_index"])
+      const index = accountIndex["account_index"]
+      if (index) {
+        const url = `https://meet.google.com/new?authuser=${index}`
+        chrome.runtime.sendMessage({ "type": "create_meet", "link": url }, (res) => {
+          console.log(res)
+        })
+      }
+    }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/pages/Popup/Popup.jsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React!
-        </a>
-      </header>
+      <p className='title'>Create Your meet Here</p>
+
+      <div className='container'>
+        {selectedAccount ?
+          <div style={{ display: "flex", justifyContent: "center", flexDirection: "column", alignItems: "center" }}>
+            <code className='email'>{`Using Account ${selectedAccount}`}</code>
+            <div style={{ marginTop: 20 }}>
+              <button onClick={createMeet} className='button'> Create Meet </button>
+            </div>
+          </div>
+          :
+          availableAccounts && (
+            <div>
+              <select onChange={(e) => setAccountIndex(e.target.value)} value={accountIndex} placeholder='Select Account'>
+                <option disabled value={-1}>Select Account</option>
+                {(availableAccounts || []).map((account, index) => {
+                  return (<option key={account} value={index}>{account}</option>)
+                })}
+              </select>
+
+              <button onClick={chooseAccount} className='button' style={{ display: "block", margin: '10px auto' }}>Choose Account</button>
+            </div>
+          )
+        }
+      </div>
+      {selectedAccount || availableAccounts ? <button style={{ marginTop: 40 }} onClick={deleteAccount} className='button'>Log Out</button> :
+
+        <button onClick={doLogin} className='button'>Login</button>
+      }
     </div>
   );
 };
